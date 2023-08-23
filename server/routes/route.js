5754d4a634411c6bc64bf95eb55860e64487e0d1
DC-21 <cholahkuboko@gmail.com>
2023-08-23 09:26:54 +0200

    first commit

--- server/routes/route.js
+++ server/routes/route.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { Student, getResults, Lecturer, getLecturers, createResults, getStudents, updateLecturer, updateStudent } = require('../controller/controller');
+const { Student, getResults, Lecturer, getLecturers, createResults, getStudents, updateLecturer, updateStudent, updateResults } = require('../controller/controller');
 
 router.post('/student', Student);
 router.get('/student', getStudents);
@@ -8,6 +8,7 @@ router.put('/student/:regno',updateStudent);
 
 router.get('/results/:regno',getResults);
 router.post('/results',createResults);
+router.put('/results/:regno',updateResults);
 
 router.post('/lecturer', Lecturer);
 router.get('/lecturer',getLecturers);
