84eb8ded54d28a6e642e370d74ae78417c8a58e7
DC-21 <cholahkuboko@gmail.com>
2023-08-24 14:36:31 +0200

    first commit

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,7 +1,9 @@
 
 const Login = () => {
   return (
-    <div className='bg-green-200 flex w-full h-screen'>Login</div>
+    <div className='bg-gradient-to-r from-purple-300 to-blue-200 flex w-full h-screen justify-center items-center'>
+        <div className="w-[380px] h-[500px] bg-white justify-center rounded shadow-lg shadow-blue-400"></div>
+    </div>
   )
 }
 
