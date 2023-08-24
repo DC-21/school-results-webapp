3e687e1607e78010798451a685c56c41115ea45f
DC-21 <cholahkuboko@gmail.com>
2023-08-24 14:43:58 +0200

    first commit

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -2,7 +2,17 @@
 const Login = () => {
   return (
     <div className='bg-gradient-to-r from-purple-300 to-blue-200 flex w-full h-screen justify-center items-center'>
-        <div className="w-[380px] h-[500px] bg-white justify-center rounded shadow-lg shadow-blue-400"></div>
+        <div className="w-[380px] flex flex-col h-[500px] bg-white justify-center items-center rounded shadow-lg shadow-blue-400">
+            <p className="flex text-center">Login PLease</p>
+            <div className="w-full flex flex-col items-center mt-2">
+                <label>Register Number</label>
+                <input style={{ textAlign:"center"}} type="number" placeholder="register number" className="border border-blue-950 rounded h-10 mt-4" />
+            </div>
+            <div className="w-full flex flex-col items-center mt-2">
+                <label>Password</label>
+                <input style={{ textAlign:"center"}} type="password" placeholder="password" className="border border-blue-950 rounded h-10 mt-4" />
+            </div>
+        </div>
     </div>
   )
 }
