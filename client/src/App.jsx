1fa92376d38b6b7d8a6336f23b470e98ba8b06ba
DC-21 <cholahkuboko@gmail.com>
2023-08-24 18:09:31 +0200

    first commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,15 @@
+import { BrowserRouter as Router,Routes, Route } from "react-router-dom"
+import Landing from "./components/Landing"
 import Login from "./components/Login"
 
 const App = () => {
   return (
     <>
-      <Login/>
+      <Router>
+      <Routes>
+        <Route path="/" element={<Landing/>}/>
+      </Routes>
+      </Router>
     </>
   )
 }
