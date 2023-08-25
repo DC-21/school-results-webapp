04b44a2406fc1ebf60399f63e69ad91838b70b57
DC-21 <cholahkuboko@gmail.com>
2023-08-25 19:02:09 +0200

    first commit

--- client/src/components/Profile.jsx
+++ client/src/components/Profile.jsx
@@ -1,8 +1,18 @@
-
 const Profile = () => {
   return (
-    <div>Profile</div>
-  )
-}
+    <div className="w-full px-4 py-2 flex justify-center items-center">
+      <div className="w-[500px] bg-blue-200 px-2  flex flex-col">
+        <div className="w-full flex">
+        <p className="w-full flex text-start">Name:</p>
+        <p className="w-full flex text-start">Chola Kuboko</p>
+        </div>
+        <div className="w-full flex">
+        <p className="w-full flex text-start">Register Number:</p>
+        <p className="w-full flex text-start">20211055021</p>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Profile
+export default Profile;
