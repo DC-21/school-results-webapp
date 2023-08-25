aa8e777b5ae0b933b510fe184751d0a6a0170e5a
DC-21 <cholahkuboko@gmail.com>
2023-08-25 14:56:33 +0200

    first commit

--- client/src/components/Landing.jsx
+++ client/src/components/Landing.jsx
@@ -11,29 +11,24 @@ const Landing = () => {
       const response = await axios.get(
         `http://localhost:3000/results/${regno}`,
         {
-          params: {
+          data: {
             semester: selectedSemester,
           },
         }
       );
-  
-      // Extract data from the response
+
       const data = response.data;
-  
-      // Update the state with the fetched data
       setResults(data);
-  
+
       console.log("Fetched Results:", data.results);
     } catch (error) {
       console.error("An error occurred:", error);
     }
   };
-  
+
   useEffect(() => {
     fetchStudentResults();
   }, [selectedSemester]);
-  
-
 
   return (
     <div className="w-full h-screen flex flex-col px-4 py-4 justify-center items-center">
@@ -54,21 +49,41 @@ const Landing = () => {
           <table className="w-full table-auto border border-green-700">
             <thead>
               <tr className="bg-green-400">
-                <th className="border border-green-700 px-2 py-2 text-white">Semester</th>
-                <th className="border border-green-700 px-2 py-2 text-white">Course</th>
-                <th className="border border-green-700 px-2 py-2 text-white">Module Code</th>
-                <th className="border border-green-700 px-2 py-2 text-white">Module Name</th>
-                <th className="border border-green-700 px-2 py-2 text-white">Mark</th>
+                <th className="border border-green-700 px-2 py-2 text-white">
+                  Semester
+                </th>
+                <th className="border border-green-700 px-2 py-2 text-white">
+                  Course
+                </th>
+                <th className="border border-green-700 px-2 py-2 text-white">
+                  Module Code
+                </th>
+                <th className="border border-green-700 px-2 py-2 text-white">
+                  Module Name
+                </th>
+                <th className="border border-green-700 px-2 py-2 text-white">
+                  Mark
+                </th>
               </tr>
             </thead>
             <tbody>
               {results.map((result, index) => (
                 <tr key={index}>
-                  <td className="border border-gray-300 px-2 py-2">{result.semester}</td>
-                  <td className="border border-gray-300 px-2 py-2">{result.course}</td>
-                  <td className="border border-gray-300 px-2 py-2">{result.code}</td>
-                  <td className="border border-gray-300 px-2 py-2">{result.name}</td>
-                  <td className="border border-gray-300 px-2 py-2">{result.mark}</td>
+                  <td className="border border-gray-300 px-2 py-2">
+                    {result.semester}
+                  </td>
+                  <td className="border border-gray-300 px-2 py-2">
+                    {result.course}
+                  </td>
+                  <td className="border border-gray-300 px-2 py-2">
+                    {result.code}
+                  </td>
+                  <td className="border border-gray-300 px-2 py-2">
+                    {result.name}
+                  </td>
+                  <td className="border border-gray-300 px-2 py-2">
+                    {result.mark}
+                  </td>
                 </tr>
               ))}
             </tbody>
