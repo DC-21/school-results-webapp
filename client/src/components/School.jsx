804fce9ae0b9f69fe0f4e2f92eb707228c53463e
DC-21 <cholahkuboko@gmail.com>
2023-08-26 22:36:26 +0200

    first commit

--- client/src/components/School.jsx
+++ client/src/components/School.jsx
@@ -3,9 +3,9 @@ import logo from '../images/logogb.png'
 const Profile = () => {
   return (
     <div className="w-full py-3 px-4 bg-green-100 flex flex-col justify-center items-center">
-        <img className='h-[150px] w-[150px]' src={logo} />
-        <p>DMI ST EUGENE UNIVERSITY-CHIBOMBO</p>
-        <p>Computer Science and Information Technology</p>
+        <img className='md:h-[150px] h-[100px] md:w-[150px] w-[90px]' src={logo} />
+        <p className='md:text-[20px] text-[15px] text-center'>DMI ST EUGENE UNIVERSITY-CHIBOMBO</p>
+        <p className='text-center'>Computer Science and Information Technology</p>
     </div>
   )
 }
