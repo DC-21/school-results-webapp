223ee53e0698e19f79878ba4c5a1b34a3ae356b9
DC-21 <cholahkuboko@gmail.com>
2023-08-26 22:32:47 +0200

    first commit

--- client/src/components/Profile.jsx
+++ client/src/components/Profile.jsx
@@ -1,7 +1,7 @@
 const Profile = () => {
   return (
     <div className="w-full px-4 py-2 flex justify-center items-center">
-      <div className="w-[380px] ml-10 px-2 flex flex-col justify-start">
+      <div className="w-[380px] md:ml-10 ml-0 px-2 flex flex-col justify-start">
         <div className="w-full flex ml-5">
         <p className="w-full flex text-start">Name:</p>
         <p className="w-full flex text-start">Chola Kuboko</p>
