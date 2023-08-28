a2d602c1d148f1f57fb88ac4abfba209b078f2b7
DC-21 <cholahkuboko@gmail.com>
2023-08-28 14:35:13 +0200

    first commit

--- lecturer/src/components/Sidebar.jsx
+++ lecturer/src/components/Sidebar.jsx
@@ -1,14 +1,14 @@
 
 const Sidebar = () => {
   return (
-    <div className="w-[150px] h-screen px-4 py-2 bg-green-500">
-        <p>Zulu</p>
-        <div className="w-full justify-center flex flex-col text-white font-medium">
-            <a>My Class</a>
-            <a>Enter Results</a>
-            <a>Update Results</a>
+    <div className="w-[150px] h-screen py-2 bg-green-500 justify-between flex flex-col">
+        <div className="w-full justify-start flex flex-col text-white font-medium">
+            <a className="hover:bg-green-700 px-1 py-2">My Class</a>
+            <a className="hover:bg-green-700 px-1 py-2">Enter Results</a>
+            <a className="hover:bg-green-700 px-1 py-2">Update Results</a>
             <a></a>
         </div>
+        <p>Zulu</p>
     </div>
   )
 }
