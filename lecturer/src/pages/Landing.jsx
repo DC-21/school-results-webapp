fc1acc1a06e3a07b9423f636c240aa56b8f83ac1
DC-21 <cholahkuboko@gmail.com>
2023-09-01 21:08:27 +0200

    first commit

--- lecturer/src/pages/Landing.jsx
+++ lecturer/src/pages/Landing.jsx
@@ -25,6 +25,10 @@ const Landing = () => {
         <div className="w-full fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
           <div className="bg-white p-4 rounded-md w-[700px] items-center flex flex-col">
             <h2 className="text-xl font-semibold mb-4">Add Student</h2>
+            <div className='w-full flex gap-4'>
+              <label>Name</label>
+              <input className='h-8 border border-black rounded'/>
+            </div>
             <button className="bg-blue-500 text-white px-4 py-2 rounded-md" onClick={closeAddStudentPopup}>
               Close
             </button>
