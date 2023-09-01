fd09d7c846f3037715d9fc16bcaa965b0219241f
DC-21 <cholahkuboko@gmail.com>
2023-09-01 20:55:38 +0200

    first commit

--- lecturer/src/pages/Landing.jsx
+++ lecturer/src/pages/Landing.jsx
@@ -3,8 +3,8 @@ const Landing = () => {
   return (
     <div className="w-full h-screen flex justify-center">
       <div className="w-full items-center justify-center flex gap-4">
-        <div className="py-4 px-2 bg-green-600 rounded text-white text-lg">Add Results</div>
-        <div className="py-4 px-2 bg-green-600 rounded text-white text-lg">Add Results</div>
+        <div className="py-4 px-2 w-[150px] bg-green-600 rounded text-white text-lg text-center">Add Results</div>
+        <div className="py-4 px-2 bg-orange-400 rounded text-white text-lg">Update Results</div>
       </div>
     </div>
   );
