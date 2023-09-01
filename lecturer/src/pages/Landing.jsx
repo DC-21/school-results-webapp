dd4e487a543ab35ee26ef2dfda126fb2090d7034
DC-21 <cholahkuboko@gmail.com>
2023-09-01 21:13:42 +0200

    first commit

--- lecturer/src/pages/Landing.jsx
+++ lecturer/src/pages/Landing.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState } from "react";
 
 const Landing = () => {
   const [isAddStudentOpen, setIsAddStudentOpen] = useState(false);
@@ -13,25 +13,53 @@ const Landing = () => {
   return (
     <div className="w-full h-screen flex flex-col justify-center gap-4">
       <div className="w-full items-center justify-center flex gap-4">
-        <div onClick={openAddStudentPopup} className="py-4 px-2 w-[150px] bg-green-600 rounded text-white text-lg text-center">Add Results</div>
-        <div className="py-4 px-2 w-[150px] bg-orange-400 rounded text-white text-lg">Update Results</div>
+        <div
+          onClick={openAddStudentPopup}
+          className="py-4 px-2 w-[150px] bg-green-600 rounded text-white text-lg text-center"
+        >
+          Add Results
+        </div>
+        <div className="py-4 px-2 w-[150px] bg-orange-400 rounded text-white text-lg">
+          Update Results
+        </div>
       </div>
       <div className="w-full items-center justify-center flex gap-4">
-        <div className="py-4 px-2 w-[150px]  bg-orange-400 rounded text-white text-lg text-center">Add Student</div>
-        <div className="py-4 px-2 w-[150px] bg-green-600 rounded text-white text-lg">Update Details</div>
+        <div className="py-4 px-2 w-[150px]  bg-orange-400 rounded text-white text-lg text-center">
+          Add Student
+        </div>
+        <div className="py-4 px-2 w-[150px] bg-green-600 rounded text-white text-lg">
+          Update Details
+        </div>
       </div>
       {/* Add Student Pop-up */}
       {isAddStudentOpen && (
         <div className="w-full fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
-          <div className="bg-white p-4 rounded-md w-[700px] items-center flex flex-col">
+          <div className="bg-white p-4 rounded-md w-[900px] items-center flex flex-col">
             <h2 className="text-xl font-semibold mb-4">Add Student</h2>
-            <div className='w-full md:flex flex-col'>
-            <div className='w-full flex gap-4 m-4'>
-              <label>Full Name</label>
-              <input style={{textAlign:"center"}} placeholder='Full name' className='h-8 border border-black rounded'/>
+            <div className="w-full flex md:flex-row flex-col">
+              <div className="w-full flex gap-4 m-4 justify-start">
+                <label>Register Number:</label>
+                <input
+                  style={{ textAlign: "center" }}
+                  placeholder="Full name"
+                  type="number"
+                  className="h-8 border border-black rounded"
+                />
+              </div>
+              <div className="w-full flex gap-4 m-4 justify-start">
+                <label>Full Name:</label>
+                <input
+                  style={{ textAlign: "center" }}
+                  placeholder="Full name"
+                  type="name"
+                  className="h-8 border border-black rounded"
+                />
+              </div>
             </div>
-            </div>
-            <button className="bg-blue-500 text-white px-4 py-2 rounded-md" onClick={closeAddStudentPopup}>
+            <button
+              className="bg-blue-500 text-white px-4 py-2 rounded-md"
+              onClick={closeAddStudentPopup}
+            >
               Close
             </button>
           </div>
