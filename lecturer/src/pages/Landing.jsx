67ea22175908909e68c6a8de90e374609b18d519
DC-21 <cholahkuboko@gmail.com>
2023-09-02 21:29:43 +0200

    first commit

--- lecturer/src/pages/Landing.jsx
+++ lecturer/src/pages/Landing.jsx
@@ -72,21 +72,14 @@ const Landing = () => {
       {isAddResults && (
         <div className="w-full fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
           <div className="bg-white p-4 rounded-md w-[600px] items-center flex flex-col">
-            <h2 className="text-xl font-semibold mb-4">Add Student</h2>
-            <div className="w-full justify-center items-center flex flex-col">
-              <div className="w-full flex md:flex-row flex-col justify-center gap-4">
-                <div className="w-full flex flex-col gap-4 justify-center">
-                  <label className="w-full md:text-start text-center">
-                    Search Student:
-                  </label>
-                  <input
-                    style={{ textAlign: "center" }}
-                    placeholder="Register number"
-                    type="number"
-                    className="h-10 border border-black rounded"
-                  />
-                </div>
-              </div>
+            <div className="w-full flex flex-col gap-4 justify-center ">
+              <p className="w-full flex text-center">Search Student:</p>
+              <input
+                style={{ textAlign: "center" }}
+                placeholder="Register number"
+                type="number"
+                className="h-10 border border-black rounded"
+              />
             </div>
             <div className="w-full flex md:flex-row flex-col mt-4 gap-4 justify-center items-center">
               <button
