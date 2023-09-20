8bb31e71b7add16e2d19437c93478d2b9b653dcc
DC-21 <cholahkuboko@gmail.com>
2023-09-20 14:26:29 +0200

    first commit

--- lecturer/src/components/Navbar.jsx
+++ lecturer/src/components/Navbar.jsx
@@ -1,18 +1,18 @@
-import logo from '../images/dmi.png'
+import logo from "../images/dmi.png";
 
 const Navbar = () => {
   return (
     <div className="w-full justify-center items-center px-4 py-2 flex bg-green-700">
-        <div className="w-full flex justify-between items-center">
-        <img className='w-[100px] h-[80px] rounded' src={logo} alt="logo"/>
-            <a className='text-white'>DMI ST EUGENE UNIVERSITY</a>
-            <div className="gap-4 flex">
-                <a className='text-white'>My Class</a>
-                <a className='text-white'>Profile</a>
-            </div>
+      <div className="w-full flex justify-between items-center">
+        <img className="w-[100px] h-[80px] rounded" src={logo} alt="logo" />
+        <a className="text-white text-xl">DMI ST EUGENE UNIVERSITY</a>
+        <div className="gap-4 flex">
+          <a className="text-white text-lg">My Class</a>
+          <a className="text-white text-lg">Profile</a>
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
