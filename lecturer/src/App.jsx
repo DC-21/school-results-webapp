4d080832e7760182af0cbc24f1eee01d0873eb29
DC-21 <cholahkuboko@gmail.com>
2023-09-20 14:23:11 +0200

    first commit

--- lecturer/src/App.jsx
+++ lecturer/src/App.jsx
@@ -1,8 +1,8 @@
 import { useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./components/Login";
-import Landing from "./pages/Landing";
 import Sidebar from "./components/Sidebar";
+import Home from "./pages/Home";
 
 const App = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
@@ -14,7 +14,7 @@ const App = () => {
       {isLoggedIn && <Sidebar />}
         <Routes>
           <Route path="/" element={<Login setIsLoggedIn={setIsLoggedIn} />} />
-          <Route path="/home" element={<Landing />} />
+          <Route path="/home" element={<Home/>} />
         </Routes>
       </div>
     </Router>
